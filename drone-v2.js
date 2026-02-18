253104c0876a510e97282ea9ea3e65fa9317497b
webflow-zypsy <webflow@zypsy.com>
2026-02-18 16:58:20 -0300

    Update drone-v2.js

--- drone-v2.js
+++ drone-v2.js
@@ -628,8 +628,8 @@ const _clearColor = new THREE.Color()
 // Assets
 // =============================================================================
 
-const HDR_URL   = "https://webflow-zypsy.github.io/icarus/bg-img.hdr"
-const EXR_URL   = "https://webflow-zypsy.github.io/icarus/bg-exr.exr"
+const HDR_URL   = "https://webflow-zypsy.github.io/icarus/green-512.hdr"  // PBR env reflections on drone materials
+const EXR_URL   = "https://webflow-zypsy.github.io/icarus/green-2k.exr"   // visible sky background + topo grid displacement
 const MODEL_URL = "https://webflow-zypsy.github.io/icarus/drone-apollo.glb"
 
 const MODEL_TUNING = {
