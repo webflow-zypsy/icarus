8eddc0f43f4943a2486ace0f00624e6268ce49cb
webflow-zypsy <webflow@zypsy.com>
2026-04-20 17:04:25 -0300

    Adjust scrollTrigger start position to 'top 5%'

--- topography-v3.js
+++ topography-v3.js
@@ -220,7 +220,7 @@ window.addEventListener("DOMContentLoaded", () => {
     ease: "none",
     scrollTrigger: {
       trigger: "[data-strat-track]",
-      start: "top top",
+      start: "top 5%",
       end: "bottom center",
       scrub: 1.5,
       invalidateOnRefresh: true,
